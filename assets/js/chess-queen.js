90d8f5eadf5341bbaf6fef966b53f6230ed75d8b
Anastasiya Melnikova (work) <sav20043@yandex.ru>
2023-09-06 16:09:44 +0300

    Шахматную задачу про 8 ферзей вынес в отдельный файл

--- assets/js/chess-queen.js
+++ assets/js/chess-queen.js
@@ -1,24 +1,25 @@
 "strict mode";
 
 
- createChessDesk(1, "result");
+ createChessDesk(1, "chess-queen-result");
 
 function createChessDesk(deskNum, divID){
   let squareColor;
   let desk="";
-  for(let i=0;i<7;++i){
+  
+  for(let i=0;i<8;++i){
     let squareRow="";
-    for(let j=0;j<7;++j){
+    for(let j=0;j<8;++j){
       if((i+j)%2){
         squareColor="black-square";
       }
       else{
         squareColor="white-square";
       }
-      squareRow=+`<div class="chess-square ${squareColor}" id="d${deskNum}r${i}c${j}"></div>`;
+      squareRow+=`<div class="chess-square ${squareColor}" id="d${deskNum}r${i}c${j}"></div>`;
     }
-    desk+=`<div class="chess-row" id="d${deskNum}r${i}"></div>`;
+    desk+=`<div class="chess-row" id="d${deskNum}r${i}">${squareRow}</div>`;
   }
-  desk=`<div class="chess-desk" id="d${deskNum}"></div>`;
+  desk=`<div class="chess-desk" id="d${deskNum}">${desk}</div>`;
   document.getElementById(divID).insertAdjacentHTML("beforeend",desk);
 }
